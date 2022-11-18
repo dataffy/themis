016d9ca83af22bef67b9dccef618005273d41170
RaduMolnar99 <53471815+RaduMolnar99@users.noreply.github.com>
2022-11-18 14:35:30 +0200

    fix: Nested validation to run schema validation (#38)

--- src/schema/schema.ts
+++ src/schema/schema.ts
@@ -110,7 +110,17 @@ export class Schema<T, Context = unknown> {
         this.options
       );
 
-      const validatorErrors = await validator.validateFields();
+      let validatorErrors: ValidationErrors = {};
+
+      try {
+        await validator.validate();
+      } catch (e) {
+        if (e instanceof ValidationError) {
+          validatorErrors = e.errors;
+        } else {
+          throw e;
+        }
+      }
 
       if (Object.keys(validatorErrors).length !== 0) {
         errors[validatorProperty] = validatorErrors;
